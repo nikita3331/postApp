9144d59a68120fad2ba0f943a6476af8e0778f12
nikita3331 <macbook>
2020-11-11 09:44:13 +0100

    fixed on IOS

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Main from './screens/Main'
 import ShowPost from './screens/ShowPost'
 import AddPost from './screens/AddPost'
 
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 const middleware = applyMiddleware(thunkMiddleware)
@@ -16,15 +17,17 @@ const store = createStore(reducer, middleware)
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="ShowPost" component={ShowPost} />
-          <Stack.Screen name="AddPost" component={AddPost} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <SafeAreaView style={{flex:1}} edges={['right', 'top', 'left']} >
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Main" component={Main} />
+            <Stack.Screen name="ShowPost" component={ShowPost} />
+            <Stack.Screen name="AddPost" component={AddPost} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </SafeAreaView>
   );
 };
 
